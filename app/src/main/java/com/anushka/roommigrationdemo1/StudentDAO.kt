63f77c1219795badb5025a58edd69d54d6795f67
preslybenoit <okebenoithub@gmail.com>
2023-09-16 00:44:47 +0100

    Room Auto Migrations

---
+++ app/src/main/java/com/anushka/roommigrationdemo1/StudentDAO.kt
@@ -0,0 +1,10 @@
+package com.anushka.roommigrationdemo1
+
+import androidx.lifecycle.LiveData
+import androidx.room.*
+
+@Dao
+interface StudentDAO {
+    @Insert
+    suspend fun insertStudent(student: Student)
+}
